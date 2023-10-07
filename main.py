42f007cdc095ffb5c4e12ec01ed124c5f57cea08
blandineruth <104473080+blandineruth@users.noreply.github.com>
2023-10-07 08:39:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+print("Getting to know you!")
+
+YourName = input("What is your name?")
+Hungry = input ("What is your favorite food?")
+Music = input("What is your favorite music?")
+WhereAreYou = input("Where are you?")
+
+print("You are")
+print(YourName)
+print() 
+
+print("You're probably hungry for")
+print(Hungry)
+print()
+print("You're probably listening to")
+print (Music)
+print()
+print ("You're probably living in the amazing")
+print (WhereAreYou)
+print() 
+print ("Have a great day!")
+
+print()
+
+print("Getting your familly !")
+
+YourName = input("What is your name familly?")
+NumberFamilly = input ("How many number personnes your familly?")
+
+print("My name familly is :  ")
+print(YourName)
+print()
+
+print("we are")
+print(NumberFamilly)
+print()
+
+print ("Have a great day!")
