b04d40fd9e5a089f6d23b5e912dde6ade3b923fe
ayiteruth004 <27179565-ayiteruth004@users.noreply.replit.com>
2023-10-07 07:13:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Je suis ABR une jeune passionnée de la programmation python ")
+print("je me suis inscrite le jeudi 05 octobre 2023")
+print(
+    """ si je prends quelque minute  par jour pour ecrit un  petit code en python c'est sur que je serai meilleur après les 100 jours."""
+)
+print(
+    "je suis très heureuse, mais jai pas pu mettre des emoji pour deinir mes sentiments."
+)
