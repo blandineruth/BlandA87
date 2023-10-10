8b0bc4c4837290b17a12d65b7ab13bfac258d866
blandineruth <104473080+blandineruth@users.noreply.github.com>
2023-10-10 05:46:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+drink = input("Do you prefer coffee or tea?")
+if drink == "coffee":
+  print("Tea is better.")
+else:
+  print("Excellent choice.")
+
+print()
+
+print("Marvel Movie Character Creator")
+print()
+
+creator = input("Do you like 'hanging around'? ")
+print()
+if creator == "yes":
+  print("You should like superheroes")
+  print()
+else:
+  print("i am hanging around")
+  print()
+
+voice =  input(" do you have a 'gravelly' voice ")
+if voice == "yes":
+  print(" then you're not korg !")
+  print()
+else:
+  print("Aww, my voice is gravelly")
