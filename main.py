056a5e6cf3155519f28e1c75448c38a012a1e631
blandineruth <104473080+blandineruth@users.noreply.github.com>
2023-10-11 05:48:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+
+tvShow = input("What is your favourite tv show? ")
+if tvShow == "peppa pig":
+  print("Ugh, why?")
+  faveCharacter = input("Who is your favourite character? ")
+  if faveCharacter == "daddy pig":
+    print("Right answer")
+  else:
+   print("Nah, Daddy Pig's the greatest")
+elif tvShow == "paw patrol":
+   print("Aww, sad times")
+else:
+   print("Yeah, that's cool and all…")
+
+
+       
+# challenge jour 7
+ 
+fav_tv = input("What is your favorite TV show, movie, or hobby?")
+if fav_tv == "tv show":
+    print("Great! Your favorite ")
+    super = input("Do you remember who sat on the Iron Throne at the end of the series?")
+    print()
+    if super == "yes":
+        print("You are correct")
+        fav_tv = input("Who is the main antagonist in The Lord of the Rings?")
+        print()
+        if fav_tv == "Frodo":
+            print("You might be a true superfan of The Lord of the Rings")
+            hobby = input("Do you like to cook or play football?")
+            print()
+            if hobby == "cook":
+                print("You are a foodie!")
+elif fav_tv == "hobby":
+    print("Interesting! What's your favorite hobby?")
+else:
+    print("Thanks")
+     
+      
+  
+  
