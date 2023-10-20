140e190bade553cb6710d964e118fbfbaa77c464
blandineruth <104473080+blandineruth@users.noreply.github.com>
2023-10-20 06:03:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# counter = 0
+# while counter < 6:
+#   print(counter)
+#   counter +=1
+
+# exit = ""
+# while exit != "yes":
+#   print("🥳")
+# exit = input("Exit?: ")
+
+# counter = 0
+# while counter < 25:
+#   print(counter)
+#   counter +=5
+
+# counter = 0
+# while counter <=12:
+#   print(counter)
+#   counter +=1
+
+# exit = ""
+# while exit != "yes":
+#   print("🥳")
+# exit = input("Exit?: ")
+
+
+exit = "no"
+
+
+while exit == "no":
+  animal_sound = input("What animal sound do you want to hear?")
+
+  if animal_sound == "cow" or animal_sound == "Cow":
+    print("🐮 Moo")
+  elif animal_sound == "pig" or animal_sound == "Pig":
+    print ("🐷 Oink")
+  elif animal_sound == "sheep" or animal_sound == "Sheep":
+    print ("🐑 Baaa")
+  elif animal_sound == "duck" or animal_sound == "Duck":
+    print("🦆 Quack")
+  elif animal_sound == "dog" or animal_sound == "Dog":
+    print("🐶 Woof")
+  elif animal_sound == "cat" or animal_sound == "Cat":
+    print("🐱 Meow")
+  else: 
+    print("I don't know that animal sound. Try again.")
+
+
+  exit = input("Do you want to exit?: ")
